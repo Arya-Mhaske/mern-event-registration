a5cd7ba424e132c9c3126fc4c6563e0304c97e27
Arya Mhaske <157601443+Arya-Mhaske@users.noreply.github.com>
2026-05-14 16:45:08 +0530

    update apiOrigin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
-const apiOrigin = (import.meta.env.VITE_API_URL || "").replace(/\/$/, "");
+const apiOrigin = "";
 const EVENTS_URL = `${apiOrigin}/api/events`;
 const REG_URL = `${apiOrigin}/api/registrations`;
 
